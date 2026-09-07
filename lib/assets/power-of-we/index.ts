f5419b7e9231640573b391a68076cafee6f99804
Shouqi Han <129344658+HSQ0503@users.noreply.github.com>
2026-09-07 15:00:52 -0400

    Fix Power of WE photo asset (split base64 parts; was truncated)

---
+++ lib/assets/power-of-we/index.ts
@@ -0,0 +1,14 @@
+import { P0 } from "./part-00";
+import { P1 } from "./part-01";
+import { P2 } from "./part-02";
+import { P3 } from "./part-03";
+import { P4 } from "./part-04";
+import { P5 } from "./part-05";
+import { P6 } from "./part-06";
+import { P7 } from "./part-07";
+import { P8 } from "./part-08";
+import { P9 } from "./part-09";
+import { P10 } from "./part-10";
+import { P11 } from "./part-11";
+
+export const POWER_OF_WE_JPEG_BASE64 = P0 + P1 + P2 + P3 + P4 + P5 + P6 + P7 + P8 + P9 + P10 + P11;
